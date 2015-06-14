64d7b4ee5020dd0dfd0c31b62ab09d86342053e3
achmadns <achmad.dev@gmail.com>
2015-06-14 23:45:32 +0700

    Add spark microservice experiment as uber jar

--- README.md
+++ README.md
@@ -1,3 +1,18 @@
 # Project Seed
 
 This repository serves me as some technology I experimented on (read: quick start project).
+
+## How to try the Spark Microframework
+
+- Make sure you have jdk 8 installed.
+
+- Run `mvn clean package` to create uber jar.
+
+- Run `java -jar -Xms16m -Xmx2048m target/simple-java-1.0-SNAPSHOT.jar` to start the app.
+
+- Open [http://localhost:4567/hello](http://localhost:4567/hello).
+
+
+When I test for some minute, the resource usage is fairly minimum (I included logback on test):
+
+![snapshot](spark-microframework-hello-world.png)
--- src/main/java/HelloWorld.java
+++ src/main/java/HelloWorld.java
@@ -1,10 +1,15 @@
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import static spark.Spark.*;
+
 public class HelloWorld {
     private static final Logger log = LoggerFactory.getLogger(HelloWorld.class);
     public static void main(String[] args) {
-        System.out.println("Hello World!");
         log.info("Hello World from logback!");
+        before("/hello", (reques, response) -> log.info("Before /hello"));
+        get("/hello", (request, response) -> "Hello World!");
+        post("/hello/:name", (request, response) -> "Hello " + request.params(":name") + "!");
+        after("/hello", ((request, response) -> log.info("After /hello")));
     }
 }
