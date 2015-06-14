2a6a4575c2f9b33fa8b900be7a022724b256a0d9
achmadns <achmad.dev@gmail.com>
2015-06-14 22:59:10 +0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Project Seed
+
+This repository serves me as some technology I experimented on (read: quick start project).
---
+++ src/main/java/HelloWorld.java
@@ -0,0 +1,10 @@
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class HelloWorld {
+    private static final Logger log = LoggerFactory.getLogger(HelloWorld.class);
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+        log.info("Hello World from logback!");
+    }
+}
