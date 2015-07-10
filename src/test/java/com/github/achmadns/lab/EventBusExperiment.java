65dcadf14297dce830471fad2846ffcfa742b457
achmadns <achmad.dev@gmail.com>
2015-07-10 11:13:30 +0700

    Alhamdulillah, it works after reviewing reactor samples on EventBus.

--- src/test/java/com/github/achmadns/lab/EventBusExperiment.java
+++ src/test/java/com/github/achmadns/lab/EventBusExperiment.java
@@ -5,6 +5,7 @@ import org.slf4j.LoggerFactory;
 import org.testng.annotations.Test;
 import org.testng.internal.thread.CountDownAdapter;
 import reactor.Environment;
+import reactor.bus.Event;
 import reactor.bus.EventBus;
 import reactor.bus.selector.Selector;
 import reactor.fn.timer.Timer;
@@ -22,6 +23,7 @@ import static reactor.bus.selector.Selectors.$;
 
 public class EventBusExperiment {
     private static final Logger log = LoggerFactory.getLogger(EventBusExperiment.class);
+    public static final String FINISHED = "finished";
     private final ExecutorService executor = Executors.newFixedThreadPool(1);
 
     static {
@@ -33,12 +35,32 @@ public class EventBusExperiment {
     public void notify_from_simple_thread_should_ok() throws InterruptedException {
         final EventBus bus = create(get());
         final CountDownAdapter waiter = new CountDownAdapter(1);
-        final Selector<String> finished = $("finished");
+        final Selector<String> finished = $(FINISHED);
         bus.on(finished, event -> waiter.countDown());
         new Thread(() -> {
             try {
                 sleep(1000);
-                bus.notify(finished);
+                bus.notify(FINISHED);
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
+        }).start();
+        waiter.await();
+    }
+
+    @Test(timeOut = 3000)
+    public void notify_from_simple_thread_using_send_and_receive_should_ok() throws InterruptedException {
+        final EventBus bus = EventBus.config().env(get()).dispatcher(Environment.SHARED).get();
+        final CountDownAdapter waiter = new CountDownAdapter(1);
+        final Selector<String> finished = $(FINISHED);
+        bus.receive(finished, event -> {
+            waiter.countDown();
+            return null;
+        });
+        new Thread(() -> {
+            try {
+                sleep(1000);
+                bus.send(FINISHED, Event.wrap("").setReplyTo(FINISHED));
             } catch (InterruptedException e) {
                 throw new RuntimeException(e);
             }
@@ -50,12 +72,12 @@ public class EventBusExperiment {
     public void notify_using_executor_should_ok() throws InterruptedException {
         final EventBus bus = create(get());
         final CountDownAdapter waiter = new CountDownAdapter(1);
-        final Selector<String> finished = $("finished");
+        final Selector<String> finished = $(FINISHED);
         bus.on(finished, event -> waiter.countDown());
         executor.execute(() -> {
             try {
                 sleep(1000);
-                bus.notify(finished);
+                bus.notify(FINISHED);
             } catch (InterruptedException e) {
                 throw new RuntimeException(e);
             }
@@ -66,7 +88,7 @@ public class EventBusExperiment {
 
     @Test(timeOut = 3000)
     public void notify_from_timer_thread_should_ok() throws InterruptedException {
-        final Selector<String> finished = $("finished");
+        final Selector<String> finished = $(FINISHED);
         final EventBus bus = create(get());
         final CountDownAdapter waiter = new CountDownAdapter(1);
         bus.on(finished,
@@ -74,12 +96,17 @@ public class EventBusExperiment {
         );
         final Timer timer = Environment.get().getTimer();
         timer.schedule(
-                aLong -> bus.notify(finished),
+                aLong -> bus.notify(FINISHED),
                 1, TimeUnit.SECONDS, 0);
         waiter.await();
     }
 
-    @Test(timeOut = 3000)
+    /**
+     * May be Promise is not suitable for notification system.
+     *
+     * @throws InterruptedException
+     */
+    @Test(timeOut = 3000, enabled = false)
     public void notify_on_promise_success_using_simple_thread_should_ok() throws InterruptedException {
         final CompleteCounterConsumer<Long> waiter = CompleteCounterConsumer.<Long>create();
         final Promise<Long> promise = Promises.<Long>prepare(get()).onComplete(waiter);
@@ -94,7 +121,12 @@ public class EventBusExperiment {
         waiter.await();
     }
 
-    @Test(timeOut = 3000)
+    /**
+     * May be Promise is not suitable for notification system.
+     *
+     * @throws InterruptedException
+     */
+    @Test(timeOut = 3000, enabled = false)
     public void notify_on_promise_success_using_executor_should_ok() throws InterruptedException {
         final CompleteCounterConsumer<Long> waiter = CompleteCounterConsumer.<Long>create();
         final Promise<Long> promise = Promises.<Long>prepare(get()).onComplete(waiter);
