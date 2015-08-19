3af268998ee2014c4208f367d6ebd3cbca53458f
Achmad Nasirudin Sandi <achmad.nasirudin@1rstwap.com>
2015-08-19 19:01:49 +0700

    Add regex selector example.

--- src/test/java/com/github/achmadns/lab/EventBusExperiment.java
+++ src/test/java/com/github/achmadns/lab/EventBusExperiment.java
@@ -2,6 +2,7 @@ package com.github.achmadns.lab;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 import org.testng.internal.thread.CountDownAdapter;
 import reactor.Environment;
@@ -12,14 +13,18 @@ import reactor.fn.timer.Timer;
 import reactor.rx.Promise;
 import reactor.rx.Promises;
 
+import java.util.concurrent.CountDownLatch;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 import java.util.concurrent.TimeUnit;
 
 import static java.lang.Thread.sleep;
+import static java.util.regex.Pattern.compile;
+import static org.assertj.core.api.Assertions.assertThat;
 import static reactor.Environment.get;
 import static reactor.bus.EventBus.create;
 import static reactor.bus.selector.Selectors.$;
+import static reactor.bus.selector.Selectors.R;
 
 public class EventBusExperiment {
     private static final Logger log = LoggerFactory.getLogger(EventBusExperiment.class);
@@ -94,7 +99,7 @@ public class EventBusExperiment {
         bus.on(finished,
                 event -> waiter.countDown()
         );
-        final Timer timer = Environment.get().getTimer();
+        final Timer timer = get().getTimer();
         timer.schedule(
                 aLong -> bus.notify(FINISHED),
                 1, TimeUnit.SECONDS, 0);
@@ -139,5 +144,34 @@ public class EventBusExperiment {
             }
         });
         waiter.await();
+    }
+
+    @Test(timeOut = 3000)
+    public void regex_should_ok() throws InterruptedException {
+        final CountDownLatch waiter = new CountDownLatch(1);
+        final EventBus bus = create(get());
+        bus.on(R("(#SET.1)(.+)"), event -> waiter.countDown());
+        bus.notify("#SET.1 bla bla bla");
+        waiter.await();
+    }
+
+    @DataProvider(name = "regex-input")
+    public Object[][] provideRegex() {
+        return new Object[][]{
+                new Object[]{"(#SET.1)(.+)", "#SET.1 bla bla bla", true},
+                new Object[]{"(#SET\\.1)(.+)", "#SET.1 bla bla bla", true},
+                new Object[]{"(#SET.1)(.+)", "#set.1 bla bla bla", false},
+                new Object[]{"((?i)#SET.1)(.+)", "#set.1 bla bla bla", true},
+                new Object[]{"((?i)#SeT.1)(.+)", "#set.1 bla bla bla", true},
+                new Object[]{"^[A-Z0-9]+$", "ABC1", true},
+                new Object[]{"^[A-Z0-9]+$", "ABC1=+", false},
+                new Object[]{"^[A-Z0-9]+$", "abc1", false},
+                new Object[]{"(?i)^[A-Z0-9]+$", "abc1", true}
+        };
+    }
+
+    @Test(dataProvider = "regex-input")
+    public void regex(String pattern, String input, boolean matches) {
+        assertThat(matches).isEqualTo(compile(pattern).matcher(input).matches());
     }
 }
