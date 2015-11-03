1cf967ace9b716eeb57fb7c40a879f09d85e7d5c
Achmad Nasirudin Sandi <achmad.nasirudin@1rstwap.com>
2015-11-03 14:58:41 +0700

    Add websocket example with ratpack.

--- src/main/java/com/github/achmadns/lab/HelloWorld.java
+++ src/main/java/com/github/achmadns/lab/HelloWorld.java
@@ -1,12 +1,80 @@
 package com.github.achmadns.lab;
 
+import com.rabbitmq.client.Channel;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.amqp.rabbit.connection.Connection;
+import org.springframework.amqp.rabbit.connection.ConnectionFactory;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.context.ApplicationListener;
+import org.springframework.context.event.ContextRefreshedEvent;
+import ratpack.spring.Spring;
+import ratpack.websocket.WebSocket;
+import ratpack.websocket.WebSocketClose;
+import ratpack.websocket.WebSocketHandler;
+import ratpack.websocket.WebSocketMessage;
 
-public class HelloWorld {
+import static ratpack.server.RatpackServer.start;
+import static ratpack.websocket.WebSockets.websocket;
+
+@EnableAutoConfiguration
+public class HelloWorld implements ApplicationListener<ContextRefreshedEvent> {
     private static final Logger log = LoggerFactory.getLogger(HelloWorld.class);
-    public static void main(String[] args) {
-        System.out.println("Hello World!");
-        log.info("Hello World from logback!");
+
+    public static void main(String[] args) throws Exception {
+        start(spec -> {
+            spec.registry(Spring.spring(HelloWorld.class));
+            spec.handlers(chain -> {
+                final ConnectionFactory connectionFactory = chain.getRegistry().get(ConnectionFactory.class);
+                final Connection connection = connectionFactory.createConnection();
+                chain.prefix("ws", ws -> {
+                    ws.all(context -> {
+                        websocket(context, new WebSocketHandler<Channel>() {
+                            @Override
+                            public Channel onOpen(WebSocket webSocket) throws Exception {
+                                final Channel channel = connection.createChannel(false);
+                                return channel;
+                            }
+
+                            @Override
+                            public void onClose(WebSocketClose<Channel> webSocketClose) throws Exception {
+                                webSocketClose.getOpenResult().close();
+                            }
+
+                            @Override
+                            public void onMessage(WebSocketMessage<Channel> webSocketMessage) throws Exception {
+                                final Channel channel = webSocketMessage.getOpenResult();
+                                final String text = webSocketMessage.getText();
+                                final WebSocket webSocket = webSocketMessage.getConnection();
+                                if (null == text || text.isEmpty()) {
+                                    webSocket.send("Message must not empty.");
+                                    return;
+                                }
+                                if (text.startsWith("send")) {
+                                    channel.basicPublish("amq.topic", "ws", null, text.getBytes());
+                                    webSocket.send("Message published.");
+                                    return;
+                                }
+                            }
+                        });
+                    });
+                });
+                chain.prefix("ping", ping -> {
+                    ping.all(context -> {
+                        context.render("pong");
+                    });
+                });
+            });
+        });
+        log.info("Server configured.");
+    }
+
+    @Override
+    public void onApplicationEvent(ContextRefreshedEvent event) {
+        log.info("Application started.");
+        try {
+        } catch (Exception e) {
+            log.error("Unable to start server", e);
+        }
     }
 }
---
+++ src/test/java/com/github/achmadns/lab/HelloWorldTest.java
@@ -0,0 +1,73 @@
+package com.github.achmadns.lab;
+
+import org.java_websocket.client.WebSocketClient;
+import org.java_websocket.handshake.ServerHandshake;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.Test;
+import ratpack.test.MainClassApplicationUnderTest;
+import reactor.Environment;
+import reactor.rx.Promise;
+import reactor.rx.Promises;
+
+import java.net.URI;
+import java.util.concurrent.CountDownLatch;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static ratpack.test.http.TestHttpClient.testHttpClient;
+
+/**
+ * Created by achmad on 03/11/15.
+ */
+public class HelloWorldTest {
+    static {
+        Environment.initializeIfEmpty();
+    }
+
+    private final MainClassApplicationUnderTest aut = new MainClassApplicationUnderTest(HelloWorld.class);
+    private final Logger log = LoggerFactory.getLogger(getClass());
+
+    @AfterClass
+    public void clean_up() {
+        aut.close();
+    }
+
+    @Test
+    public void ping_should_success() {
+        log.info("Server started at {}", aut.getAddress().toString());
+        assertThat("pong").isEqualTo(testHttpClient(aut).getText("ping"));
+    }
+
+    @Test
+    public void publish_message_should_success() throws InterruptedException {
+        final Promise<String> promise = Promises.prepare(Environment.get());
+        final CountDownLatch latch = new CountDownLatch(1);
+        promise.onComplete(promised -> latch.countDown());
+        final WebSocketClient client = new WebSocketClient(URI.create(aut.getAddress().toString() + "ws")) {
+            @Override
+            public void onOpen(ServerHandshake handshakedata) {
+                send("send some data");
+            }
+
+            @Override
+            public void onMessage(String message) {
+                promise.onNext(message);
+            }
+
+            @Override
+            public void onClose(int code, String reason, boolean remote) {
+
+            }
+
+            @Override
+            public void onError(Exception ex) {
+                promise.onError(ex);
+            }
+        };
+        client.connect();
+        latch.await();
+        assertThat(true).isEqualTo(promise.isSuccess());
+        assertThat("Message published.").isEqualTo(promise.get());
+    }
+}
